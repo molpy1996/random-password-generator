4f7f0556be89134780bf13fcbbd50bc9e6500dbb
molpy1996 <36535002+molpy1996@users.noreply.github.com>
2022-01-15 13:00:57 +0100

    genereted a random letter with random.randint and chr functions

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+# chr function return the letter corresponding to a ASCII value
+# random.randint return a random numeric value between the 2 parameters given
+uppercaseLetter=chr(random.randint(65,90))
+print(uppercaseLetter)
